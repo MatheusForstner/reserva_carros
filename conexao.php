4fe74bac5dfd5df129238e42e70b76c5fa0382cc
Matheus Forstner Larangeiro <71892466+MatheusForstner@users.noreply.github.com>
2025-04-29 11:29:28 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$servidor = 'GRPJNG011204080';  // IP do servidor MySQL
-$usuario = 'root';              // Nome de usuário do banco de dados
-$senha = '';                    // Senha do banco de dados (deixe em branco se não houver senha)
-$dbname = 'agendamentos';       // Nome do banco de dados
+$servidor = 'seu servidor';  // IP do servidor MySQL
+$usuario = 'seu usuário';              // Nome de usuário do banco de dados
+$senha = 'seu senha, se não tem senha. vazio';                    // Senha do banco de dados (deixe em branco se não houver senha)
+$dbname = 'sua tabela dados';       // Nome do banco de dados
 
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
